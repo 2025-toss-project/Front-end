25673b669d70d3faab99c2fe7eed346c7cfa9a6f
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-02-25 09:21:25 +0900

    Apply fonts to global

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const MainPage: React.FC = () => {
-  return <div className="bg-slate-200 font-bold text-red-500">PayRoad</div>;
+  return (
+    <>
+      <div className="font-light">A</div>
+      <div className="font-normal">A</div>
+      <div className="font-medium">A</div>
+      <div className="font-semibold">A</div>
+      <div className="font-bold">A</div>
+    </>
+  );
 };
 
 export default MainPage;
