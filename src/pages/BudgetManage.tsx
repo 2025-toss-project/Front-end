4b39e60160475bb7fe99c0d93db271d9a1633382
leehakseung123 <hak9802022@naver.com>
2025-02-26 16:32:32 +0900

    style : 목표 예산 관리(그래프제외)

--- src/pages/BudgetManage.tsx
+++ src/pages/BudgetManage.tsx
@@ -5,83 +5,133 @@ import {
   LucideForkKnife,
 } from "lucide-react";
 import React from "react";
+import IconFood from "../assets/categoryIcons/IconFood";
+import IconEvent from "../assets/categoryIcons/IconEvent";
 
 const BudgetManage = () => {
   return (
-    <div className="flex flex-col h-full px-6 py-5 bg-second-lighter">
+    <div className="flex h-full flex-col bg-[#f8f8f8] px-6 py-5">
+      <div className="flex h-16 w-full items-center justify-center">
+        목표예산관리
+      </div>
       {/* 1. 목표예산 설정 */}
-      <div className="flex items-center justify-between px-4 py-3 mb-4 border rounded-2xl border-second-light">
+      <div className="1px mb-4 mt-5 flex items-center justify-between rounded-2xl border border-second-light bg-white px-4 py-3">
         <div>
           <h2 className="text-lg font-bold">목표예산 설정</h2>
-          <p className="text-xs text-second">
-            목표예산을 설정하여 체계적으로 관리하세요.
-          </p>
+          <p className="text-xs">목표예산을 설정하여 체계적으로 관리하세요.</p>
         </div>
-        <ChevronRight className="w-5 h-5 text-second" />
+        <ChevronRight className="h-5 w-5" />
       </div>
 
       {/* 2. 현재 예산 사용 현황 */}
-      <div className="p-4 mb-4 border rounded-2xl border-second-light">
-        <p className="mb-1 text-lg font-semibold">
+      <div className="mb-4 rounded-2xl bg-white p-4 drop-shadow-10">
+        <p className="mb-1 text-lg font-bold">
           현재 예산의 <span className="text-main">60%</span>를 사용했어요
         </p>
-        <div className="mb-2 text-sm">
+        <div className="text-sm">
           남은 기간 동안 하루에
           <div>
-            <span>15,000원</span>을 사용할 수 있어요.
+            <span className="font-bold text-marker-home">15,000원</span>을
+            사용할 수 있어요.
           </div>
         </div>
-        <div className="flex items-center justify-center w-full h-24 rounded-md bg-second-light">
-          원형 그래프 자리
+
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <div className="text-sm">남은 금액</div>
+            <div className="text-lg font-bold text-main">15,000원</div>
+          </div>
+
+          <div className="flex flex-col">
+            <div>총 예산</div>
+            <div className="text-lg font-bold text-marker-home">200,000원</div>
+          </div>
         </div>
       </div>
 
       {/* 3. 카테고리별 소비 현황 */}
-      <div className="p-4 border rounded-2xl border-second-light">
+      <div className="gap-x gap-x-2.5 rounded-2xl bg-white px-3 py-5 drop-shadow-10">
         <div className="mb-4">
-          <h3 className="text-lg font-bold text-main">카테고리별 소비 현황</h3>
-          <p className="text-sm text-second">
+          <h3 className="text-lg font-bold">카테고리별 소비 현황</h3>
+          <p className="pb-2.5 text-sm">
             예산 대비 사용량을 한눈에 보고 관리하세요.
           </p>
         </div>
 
         {/* 예: 식비 카테고리 */}
-        <div className="mb-5">
-          <div className="flex items-center justify-between mb-1">
+        <div className="mb-5 rounded-2xl bg-[#f8f8f8] px-2 py-2.5">
+          <div className="mb-1 flex items-center justify-between pb-2">
             <div className="flex items-center space-x-2">
-              <LucideForkKnife/>
-              <span className="text-sm font-medium text-marker-food">식비</span>
+              <div className="flex rounded-full bg-second-lighter">
+                <IconFood />
+              </div>
+              <div className="text-sm font-medium">식비</div>
+              <div className="text-xs font-medium">100%</div>
             </div>
-            <span className="text-sm font-medium text-marker-food">100%</span>
+
+            <div className="text-sm font-bold">200,000원 남음</div>
           </div>
-          <p className="mb-1 text-xs text-main">20,000원 초과</p>
+
           {/* 막대바(사용률) */}
-          <div className="w-full h-2 mb-2 rounded-full bg-marker-food-light">
-            <div className="w-3/4 h-2 rounded-full bg-marker-food"></div>
+          <div className="h-2 w-full rounded-full bg-second-light">
+            <div className="h-2 w-3/4 rounded-full bg-marker-home"></div>
           </div>
-          <div className="flex justify-between text-xs text-second">
+
+          <div className="text-3 flex justify-between pb-1 pt-2 text-xs">
             <span>220,000원 지출</span>
             <span>예산 200,000원</span>
           </div>
         </div>
 
-        {/* 필요하다면 다른 카테고리들도 동일하게 반복 */}
-        <div className="mb-5">
-          <div className="flex items-center justify-between mb-1">
+        {/* ////초과 예시 */}
+
+        <div className="mb-5 rounded-2xl bg-[#f8f8f8] px-2 py-2.5">
+          <div className="mb-1 flex items-center justify-between pb-2">
             <div className="flex items-center space-x-2">
-              <LucideForkKnife className="w-4 h-4 text-marker-food" />
-              <span className="text-sm font-medium text-marker-food">식비</span>
+              <div className="flex rounded-full bg-second-lighter">
+                <IconEvent />
+              </div>
+              <div className="text-sm font-medium">경조사</div>
+              <div className="text-xs font-medium">100%</div>
             </div>
-            <span className="text-sm font-medium text-marker-food">100%</span>
+
+            <div className="text-sm font-bold">200,000원 초과</div>
           </div>
-          <p className="mb-1 text-xs text-second">200,000원 남음</p>
+
           {/* 막대바(사용률) */}
-          <div className="w-full h-2 mb-2 rounded-full bg-marker-food-light">
-            <div className="w-full h-2 rounded-full bg-marker-food"></div>
+          <div className="h-2 w-full rounded-full bg-second-light">
+            <div className="h-2 rounded-full bg-main"></div>
           </div>
-          <div className="flex justify-between text-xs text-second">
-            <span>16,200원 지출</span>
-            <span>예산 16,200원</span>
+
+          <div className="text-3 flex justify-between pb-1 pt-2 text-xs">
+            <span>220,000원 지출</span>
+            <span>예산 200,000원</span>
+          </div>
+        </div>
+
+        {/*  */}
+
+        <div className="mb-5 rounded-2xl bg-[#f8f8f8] px-2 py-2.5">
+          <div className="mb-1 flex items-center justify-between pb-2">
+            <div className="flex items-center space-x-2">
+              <div className="flex rounded-full bg-second-lighter">
+                <IconFood />
+              </div>
+              <div className="text-sm font-medium">식비</div>
+              <div className="text-xs font-medium">100%</div>
+            </div>
+
+            <div className="text-sm font-bold">200,000원 남음</div>
+          </div>
+
+          {/* 막대바(사용률) */}
+          <div className="h-2 w-full rounded-full bg-second-light">
+            <div className="h-2 w-3/4 rounded-full bg-marker-home"></div>
+          </div>
+
+          <div className="text-3 flex justify-between pb-1 pt-2 text-xs">
+            <span>220,000원 지출</span>
+            <span>예산 200,000원</span>
           </div>
         </div>
       </div>
