0c8a8888e67258e86bfb9472918e5737d03161bd
jimmiS2 <gyfl23242@naver.com>
2025-02-26 20:10:55 +0900

    style: 캘린더 패키지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import BudgetManage from "./pages/BudgetManage";
 import AddExpenditurePage from "./pages/AddExpenditurePage";
+import PayRecodePage from "./pages/PayRecodePage";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Route path="/" element={<MainPage />} />
       <Route path="/budget" element={<BudgetManage />} />
       <Route path="/addExpenditure" element={<AddExpenditurePage />} />
+      <Route path="/payrecode" element={<PayRecodePage />} />
     </Routes>
   );
 };
---
+++ src/pages/PayRecodePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from "../layouts/MainLayout";
+import Calendar from "../components/Calendar";
+
+const PayRecodePage = () => {
+  return (
+    <MainLayout>
+      <div>
+        <Calendar />
+      </div>
+    </MainLayout>
+  );
+};
+
+export default PayRecodePage;
