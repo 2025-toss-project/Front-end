aa03367d4aa88d8f1f60438c5638fb3ce23bb6a6
jimmiS2 <gyfl23242@naver.com>
2025-02-27 23:47:07 +0900

    style: 날짜별 총액 퍼블리싱

--- src/components/CustomCalendar.tsx
+++ src/components/CustomCalendar.tsx
@@ -1,6 +1,7 @@
 import { LucidePlus, LucideTriangle, LucideTriangleRight } from "lucide-react";
 import React, { useState } from "react";
 import CustomCalendar from "react-calendar";
+import { spendingData } from "../spendingData";
 
 // 캘린더 value 타입 정의
 type ValuePiece = Date | null;
@@ -15,8 +16,8 @@ const Calendar = () => {
   // 캘린더 사이 들어갈 컴포넌트
   const CalendarHeader = () => {
     return (
-      <div className="flex justify-between px-10 py-2">
-        <p> {Amount} </p>
+      <div className="flex w-full flex-row justify-between px-10 pt-3">
+        <p className=""> {Amount} </p>
         <LucidePlus />
       </div>
     );
@@ -52,7 +53,32 @@ const Calendar = () => {
         } // 이전 달 버튼
         next2Label={null} // >> 버튼 숨김
         prev2Label={null} // << 버튼 숨김
+        // 일일 소비 내역 렌더링
+        tileContent={({ date }) => {
+          const year = date.getFullYear().toString();
+          const month = (date.getMonth() + 1).toString();
+          const day = date.getDate().toString();
+
+          // 내 데이터에서 달력 날짜랑 같은 거 찾기
+          const dayRecord = spendingData.records.find(
+            (record) =>
+              record.year === year &&
+              record.month === month &&
+              record.day === day,
+          );
+
+          return (
+            <div className="relative flex h-full flex-col justify-between pt-2">
+              {dayRecord && (
+                <div className="text-red absolute bottom-2.5 left-1/2 -translate-x-1/2 text-xs active:text-white">
+                  {dayRecord.dailyTotal.toLocaleString()}
+                </div>
+              )}
+            </div>
+          );
+        }}
       />
+
       {/* 네비게이션과 날짜 사이에 금액 표시 (달리 방법이 없어서 absol로 구현) */}
       {calAmount > 0 && (
         <div className="absolute top-28 w-full text-left text-lg font-medium">
