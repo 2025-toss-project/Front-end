5f9b633746ace3f2ff25bf5c6dbb115b164753e4
heedong12 <dlgmldus3289@naver.com>
2025-02-28 15:57:18 +0900

    feat :  주소검색 페이지에 헤더 추가

--- src/pages/SearchLocation.tsx
+++ src/pages/SearchLocation.tsx
@@ -1,11 +1,16 @@
+import { LucideX } from "lucide-react";
 import DaumPostcodeEmbed from "react-daum-postcode";
+import Header from "../components/common/Header";
 
 const SearchLocation = () => {
   const onComplete = (data: any) => {
     console.log(data.jibunAddress);
   };
   return (
-    <DaumPostcodeEmbed style={{ height: "100%" }} onComplete={onComplete} />
+    <div className="flex h-full w-full flex-col">
+      <Header title="주소검색" />
+      <DaumPostcodeEmbed style={{ height: "100%" }} onComplete={onComplete} />
+    </div>
   );
 };
 
