55302a0e0f39b3ff851113157869eb2bb82cf5a4
heedong12 <dlgmldus3289@naver.com>
2025-02-28 13:49:59 +0900

    style : 마이페이지 프로필 탭 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import AddPayPage from "./pages/AddPayPage";
 import LoginPage from "./pages/LoginPage";
 import HeaderLayout from "./layouts/HeaderLayout";
 import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
+
       <Route element={<MainLayout title="예산관리" bgColor="bg-second-bg" />}>
         <Route path="/budget" element={<BudgetManage />} />
       </Route>
       <Route element={<MainLayout title="지출 내역 추가" />}>
         <Route path="/addpay" element={<AddPayPage />} />
       </Route>
+      <Route element={<MainLayout title="마이페이지" />}>
+        <Route path="/mypage" element={<MyPage />} />
+      </Route>
+
       <Route element={<HeaderLayout />}>
         <Route path="/signup" element={<SignupPage />} />
       </Route>
