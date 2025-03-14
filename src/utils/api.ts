d05a544dd4bc8b3a8ef6ab3aa7888c8fa8a6299c
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-03-14 15:33:55 +0900

    fix : api.ts 파일 추가

---
+++ src/utils/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "/api",
+});
+
+export const apiWithoutAuth = axios.create({
+  baseURL: "/api",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
